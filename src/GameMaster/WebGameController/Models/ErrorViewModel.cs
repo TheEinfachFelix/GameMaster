f48fcd5eaf8e26faa57a9a37f2a0b316778cbb8c
TheEinfachFelix <the.einfach.felix@gmail.com>
2024-06-20 22:08:26 +0200

    Removed old files

--- src/GameMaster/WebGameController/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebGameController.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
